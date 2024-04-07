530fe31710c7dc2b2fcfb57b7970ecf6365c24ec
Alex Volo <alex_volo@bk.ru>
2024-04-07 13:37:45 +0900

    init and maybe finish

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'components/UI/Button/Button.tsx'
+import { Title } from 'components/UI/Title/Title.tsx'
+import './App.scss'
+
+export const App = () => (
+  <div>
+    <Title color={'red'}>Заголовок</Title>
+    <Button>Нажми</Button>
+  </div>
+)
